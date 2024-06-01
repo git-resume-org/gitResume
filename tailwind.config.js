e744ed8fd017cca69c97c7713215bc4bbb51ec1b
Nina Skyttmo <ninaskyttmo@Ninas-MacBook-Pro-2.local>
2024-06-01 13:50:06 -0400

    added colors to tailwind conifig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   purge: ['./client/**/*.{js,jsx,ts,tsx}', './client/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      greenGR: '#62e6ce',
-      blueGR: '#62aee6',
+    extend: {
+      colors: {
+        greenGR: '#62e6ce',
+        blueGR: '#62aee6',
+      },
     },
   },
   variants: {
