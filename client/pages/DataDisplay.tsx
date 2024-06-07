9160f2e9f2ebbd852c647bd71c9fa53a77d1fa1b
Elena-Netepenko <elen.netepenko@gmail.com>
2024-06-06 20:10:32 -0700

    created html structure for Bullet Point component

--- client/pages/DataDisplay.tsx
+++ client/pages/DataDisplay.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 const DataDisplay = () => {
   return (
     <div>
-
+      <header className='p-4 text-sm text-white'>
+        <a href='/repodisplay'>← Back to repositories</a>
+      </header>
+      
+      <main>
+        <div>
+          <h1 className='text-greenGR'>Yesql</h1>
+          <ul>
+            <li className='text-white'>Bullet One</li>
+            <li className='text-white'>Bullet Two</li>
+            <li className='text-white'>Bullet Three</li>
+          </ul>
+          <button className='bg-greenGR'>Copy To Clipboard</button>
+        </div>
+      </main>
     </div>
   )
 }
