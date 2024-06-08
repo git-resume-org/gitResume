dbf5ef6636747a395ffb48d5889930f637010d72
Elena-Netepenko <elen.netepenko@gmail.com>
2024-06-07 17:05:36 -0700

    modified DataDisplay component to programmatically render bullet points based on native state

--- client/pages/DataDisplay.tsx
+++ client/pages/DataDisplay.tsx
@@ -5,7 +5,11 @@ import { useSelectedRepo } from '../components/SelectedRepoProvider';
 const DataDisplay = () => {
   const { selected } = useSelectedRepo();
   const [bulletPoints, setBulletPoints] = useState([]);
-  console.log('SELECTED:   ', selected);
+  const testBulletPoints = [
+    'Used React to develop an application which mirrors the functionality of Chrome Developer Tools Styles tab but displays source file names and line numbers for all types of CSS styles, enabling faster CSS debugging and development.',
+    'Integrated DOM and CSS domains of Chrome Developer Protocol to communicate directly with browser’s functionality without abstraction layers provided by higher-level libraries and to fetch CSS property data of the target application.',
+    'Implemented Redux Toolkit, using Immer and Redux thunks to simplify state management across multiple React components, handle asynchronous HTTP and API requests, ensure immutable state update and efficient data flow.'
+  ];
 
   useEffect(() => {
     console.log('Fetching bullet points data...');
@@ -15,12 +19,17 @@ const DataDisplay = () => {
       body: JSON.stringify({ repoName: selected })
     })
       .then(res => res.json())
-      .then(data => {
-        console.log('DATA DISPLAY COMPONENT: FETCHED DATA:   ', data);
-        setBulletPoints(data);
-      })
+      .then(data => setBulletPoints(data.bulletPoints))
       .catch(err => console.error(`Error fetching bullet points for repo ${selected}. Error: ${err}`))
   }, []);
+
+  const bulletPointElements = testBulletPoints.map((bp, idx) => (
+    <li key={`bullet-point-${idx}`} className='text-white my-4'>{bp}</li>
+  ));
+
+  const handleCopyToClipboardClick = () => {
+    console.log('Copy to clipboard btn has been clicked!');
+  };
 
   return (
     <div>
@@ -31,23 +40,15 @@ const DataDisplay = () => {
       <main className='flex items-center justify-center h-screen bg-blackGR'>
         <div className='bg-darkGrayGR w-3/4 h-4/6 rounded-3xl pt-7 px-10 font-sans flex flex-col items-center justify-between'>
           <div>
-            <h1 className='text-greenGR text-2xl mb-5'>"Yesql" repository</h1>
+            <h1 className='text-greenGR text-2xl mb-5'>"{selected}" repository</h1>
             <ul className='list-disc list-inside'>
-              <li className='text-white my-4'>
-                Used React to develop an application which mirrors the functionality of Chrome Developer Tools Styles tab but displays source file names and line numbers for all types of CSS styles, enabling faster CSS debugging and development.
-              </li>
-              <li className='text-white my-4'>
-                Integrated DOM and CSS domains of Chrome Developer Protocol to communicate directly with browser’s functionality without abstraction layers provided by higher-level libraries and to fetch CSS property data of the target application.
-              </li>
-              <li className='text-white my-4'>
-                Implemented Redux Toolkit, using Immer and Redux thunks to simplify state management across multiple React components, handle asynchronous HTTP and API requests, ensure immutable state update and efficient data flow.
-              </li>
+              {bulletPointElements}
             </ul>
           </div>
           <Button
             className="my-8 hover:bg-lavenderGR focus:bg-blueGR"
             variant='default'
-            // onClick={}
+            onClick={handleCopyToClipboardClick}
           >Copy To Clipboard</Button>
         </div>
       </main>
